10b85042a8329970857a247b750fd2ac289f6a1b
mrAJAY1 <thisismeajayprakash@gmail.com>
2023-04-26 02:13:29 +0530

    completed responsive navbar

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 // Script for navigation bar
+
+// navbar functions
+const bar = document.getElementById("bar");
+const close = document.getElementById("close");
+const nav = document.getElementById("navbar");
+if (bar) {
+  bar.addEventListener("click", () => {
+    nav.classList.add("active");
+  });
+}
+if (close) {
+  close.addEventListener("click", (e) => {
+    e.preventDefault()
+    nav.classList.remove("active");
+  });
+}
